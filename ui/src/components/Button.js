c35cfbf26be8e9af157823932e2f8502d952c5a9
Apatran <andrei.patranoiu@mail.utoronto.ca>
2016-11-19 14:07:03 -0500

    Button Component Initial Commit

---
+++ ui/src/components/Button.js
@@ -0,0 +1,26 @@
+/*
+  By Andrei Patranoiu
+*/
+
+// Dependencies
+
+import React, { Component } from 'react'
+
+// Components
+
+import { Center } from 'components/Flex'
+
+
+export class Button extends Component {
+  constructor () {
+    super()
+  }
+
+  render () {
+    return (
+      <div>
+        test
+      </div>
+    )
+  }
+}
