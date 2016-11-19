7b999a4c482fd7d4979cb7f3ca127f9725f017b1
Rachel Bk <rachelbaek@IB-Air.local>
2016-11-19 15:50:53 -0500

    preliminary chart working
    TODO
    
    -extrapolate date
    -design

--- ui/src/components/Body.js
+++ ui/src/components/Body.js
@@ -4,17 +4,58 @@ import {
   Text,
   Block,
   Container,
+  Heading,
+  Divider,
+  Grid,
+  Button,
 } from 'rebass'
 
 import { Chart } from './Charts'
 
-function BodyChart() {
-  return <Flex>
-    <Box py={4} px={4}>
-      <Chart />
+
+function Header() {
+    return<Box px={5}>
+    <Heading level={1}>Stats</Heading>
+    <p> Analysis of Statistics </p>
+    <Divider />
     </Box>
-  </Flex>
 }
 
+function SummaryInfo() {
+  return <Block
+    borderLeft
+    px={3}
+    color='primary'>
+    <Heading
+      level={2}
+      children='Blossom' />
+      <br/>
+    <Text>
+      Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aliquam dapibus tristique justo nec semper. Vestibulum ante ipsum primis in faucibus orci luctus et ultrices posuere cubilia Curae.
+    </Text>
+  </Block>
+}
 
-export default BodyChart;
+function Body() {
+  return<Container>
+  <Header />
+  <Flex
+  justify="space-around"
+>
+  <Box p={3}>
+    <SummaryInfo />
+  <Flex
+    justify="space-around">
+    <Button />
+    <Button />
+    <Button />
+  </Flex>
+  </Box>
+  <Box p={3}>
+    <Chart />
+  </Box>
+</Flex>
+</Container>
+}
+
+export default Body;
--- ui/src/components/Charts.js
+++ ui/src/components/Charts.js
@@ -25,14 +25,12 @@ const data = [
 
 export function Chart(props) {
   return <LineChart
-  width={400}
-  height={400}
+  width={600}
+  height={600}
   data={data}
   margin={{ top: 5, right: 20, left: 10, bottom: 5 }}
 >
   <XAxis dataKey="name" />
-  <Tooltip />
-  <CartesianGrid stroke="#f5f5f5" />
   <Line type="monotone" dataKey="uv" stroke="#ff7300" yAxisId={0} />
   <Line type="monotone" dataKey="pv" stroke="#387908" yAxisId={1} />
 </LineChart>
