158d57a4eb9db9845cd1366b57a7e9bfe984adb4
Rachel Bk <rachelbaek@IB-Air.local>
2016-11-20 11:41:07 -0500

    UI: load key metrics dynamically

--- ui/src/pages/Dashboard.js
+++ ui/src/pages/Dashboard.js
@@ -24,6 +24,10 @@ function getDatacenters() {
   });
 }
 
+function getKeyMetrics() {
+  return getApiData('http://localhost:8000/ads/overview');
+}
+
 function groupByDatacenter(servers) {
   return _.chain(servers)
   .groupBy('dc')
@@ -38,7 +42,7 @@ function groupByDatacenter(servers) {
 }
 
 // mock data pulled from API
-const DATA = {"message":"OK","data":{"all":[{"spend":583144755.1100008,"views":37622093,"platform":"desktop","format":"video"},{"spend":341570184.59000003,"views":78513137,"platform":"desktop","format":"banner"},{"spend":491349834.10999924,"views":36841598,"platform":"mobile","format":"video"},{"spend":292949026.95000154,"views":89266806,"platform":"mobile","format":"banner"},{"spend":463285784.0700005,"views":21300603,"platform":"app","format":"video"},{"spend":258220594.7399996,"views":49369179,"platform":"app","format":"banner"}],"NA":[{"spend":254846736.41999993,"views":14323052,"platform":"desktop","format":"video"},{"spend":172843896.82000002,"views":34522027,"platform":"desktop","format":"banner"},{"spend":187904214.75999984,"views":11449061,"platform":"mobile","format":"video"},{"spend":137293009.32999986,"views":34381845,"platform":"mobile","format":"banner"},{"spend":167449309.3600001,"views":6350041,"platform":"app","format":"video"},{"spend":103344441.49000001,"views":17232910,"platform":"app","format":"banner"}],"EU":[{"spend":273778198.82999957,"views":17211031,"platform":"desktop","format":"video"},{"spend":132249771.33999997,"views":29402176,"platform":"desktop","format":"banner"},{"spend":226919896.85999978,"views":16296447,"platform":"mobile","format":"video"},{"spend":128366572.8200001,"views":36677837,"platform":"mobile","format":"banner"},{"spend":240716876.69000003,"views":10068840,"platform":"app","format":"video"},{"spend":116678126.38000003,"views":21203713,"platform":"app","format":"banner"}],"AS":[{"spend":54519819.85999997,"views":6088010,"platform":"desktop","format":"video"},{"spend":36476516.429999985,"views":14588934,"platform":"desktop","format":"banner"},{"spend":76525722.49000005,"views":9096090,"platform":"mobile","format":"video"},{"spend":27289444.800000038,"views":18207124,"platform":"mobile","format":"banner"},{"spend":55119598.01999992,"views":4881722,"platform":"app","format":"video"},{"spend":38198026.86999996,"views":10932556,"platform":"app","format":"banner"}]}};
+//const DATA = {"message":"OK","data":{"all":[{"spend":583144755.1100008,"views":37622093,"platform":"desktop","format":"video"},{"spend":341570184.59000003,"views":78513137,"platform":"desktop","format":"banner"},{"spend":491349834.10999924,"views":36841598,"platform":"mobile","format":"video"},{"spend":292949026.95000154,"views":89266806,"platform":"mobile","format":"banner"},{"spend":463285784.0700005,"views":21300603,"platform":"app","format":"video"},{"spend":258220594.7399996,"views":49369179,"platform":"app","format":"banner"}],"NA":[{"spend":254846736.41999993,"views":14323052,"platform":"desktop","format":"video"},{"spend":172843896.82000002,"views":34522027,"platform":"desktop","format":"banner"},{"spend":187904214.75999984,"views":11449061,"platform":"mobile","format":"video"},{"spend":137293009.32999986,"views":34381845,"platform":"mobile","format":"banner"},{"spend":167449309.3600001,"views":6350041,"platform":"app","format":"video"},{"spend":103344441.49000001,"views":17232910,"platform":"app","format":"banner"}],"EU":[{"spend":273778198.82999957,"views":17211031,"platform":"desktop","format":"video"},{"spend":132249771.33999997,"views":29402176,"platform":"desktop","format":"banner"},{"spend":226919896.85999978,"views":16296447,"platform":"mobile","format":"video"},{"spend":128366572.8200001,"views":36677837,"platform":"mobile","format":"banner"},{"spend":240716876.69000003,"views":10068840,"platform":"app","format":"video"},{"spend":116678126.38000003,"views":21203713,"platform":"app","format":"banner"}],"AS":[{"spend":54519819.85999997,"views":6088010,"platform":"desktop","format":"video"},{"spend":36476516.429999985,"views":14588934,"platform":"desktop","format":"banner"},{"spend":76525722.49000005,"views":9096090,"platform":"mobile","format":"video"},{"spend":27289444.800000038,"views":18207124,"platform":"mobile","format":"banner"},{"spend":55119598.01999992,"views":4881722,"platform":"app","format":"video"},{"spend":38198026.86999996,"views":10932556,"platform":"app","format":"banner"}]}};
 
 function Datacenters(props) {
   const dcs = props.datacenters;
@@ -71,8 +75,10 @@ class App extends Component {
 
     this.state = {
       datacenters: null,
+      keyMetrics: null,
     };
     this.loadDatacenters();
+    this.loadKeyMetrics();
   }
 
   loadDatacenters() {
@@ -84,8 +90,13 @@ class App extends Component {
     })
   }
 
+  loadKeyMetrics() {
+    getKeyMetrics()
+    .then((keyMetrics) => this.setState({keyMetrics}))
+  }
+
   render() {
-    const stats = DATA.data.all;
+    const stats = this.state.keyMetrics ? this.state.keyMetrics.all : [];
 
     return (
       <Page>
